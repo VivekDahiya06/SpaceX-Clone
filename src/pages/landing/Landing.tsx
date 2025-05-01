820277ff0c38c941ec32a30143513ebd7305fb86
VivekDahiya06 <vivekdahiya312@gmail.com>
2025-05-02 00:33:06 +0530

    Added all the files to git that were given to me at the start of the project.

---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import { Title } from "@mantine/core";
+
+const Landing: FC = () => {
+	return (
+		<>
+			<Title order={4}> This is the landing page </Title>
+		</>
+	);
+};
+
+export default Landing
+
